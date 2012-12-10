d1ca159246d64a2d730c0144f17ca8a0ee868070
Alyssa Nicoll <mail@alyssanicoll.com>
2012-12-09 21:21:02 -0500

    The first Commit of facesOfMadison.com. w00t!
    details are overrated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facesofmadison::Application.config.secret_token = '7e2203b204e8ff6619526102655415304f950cbc84b5f33fe45b6e11781755fb5d19b2744b4c1cb9372eefa53d17c6c885515e4f2a3b2aa27c2fec5964ba3beb'
