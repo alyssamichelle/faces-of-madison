97b9473281ebb8c0b11667662af08b467afa2b9a
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-03 14:24:00 -0400

    cleaned up js file and added correct video route

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 
 $(function(){
   var $video = $("<div class='video' />");
-  $video.load('/videos.html #video_mov');
+  $video.load('/videos #video_mov');
 
   $("#videoContainer").prepend($video)
 
@@ -25,7 +13,7 @@ $(function(){
     var id = "video_" + getTheData;
     $("#videContainer div.video").remove();
     $video.empty();
-    $video.load('/videos.html #'+ id);
+    $video.load('/videos #'+ id);
     $("#videoContainer").prepend($video);
   });
 
