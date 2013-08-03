d45ec59eceece892a97574c779891f5069cb6ca1
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-03 13:42:42 -0400

    Added script for videos and image resizing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,34 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+  var $video = $("<div class='video' />");
+  $video.load('/videos.html #video_mov');
+
+  $("#videoContainer").prepend($video)
+
+  $("#tabContainer").on("click", ".tab", function(event){
+    var getTheData = $(event.currentTarget).data("video");
+    var id = "video_" + getTheData;
+    $("#videContainer div.video").remove();
+    $video.empty();
+    $video.load('/videos.html #'+ id);
+    $("#videoContainer").prepend($video);
+  });
+
+  var timeout;
+  $(window).on('resize', function(){
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+      resizeFaces();
+    }, 25);
+  });
+
+  var resizeFaces = function() {
+    var width = $('#imagesContainer > div:first').width();
+    $('#imagesContainer > div').css('height', width);
+  }
+
+  resizeFaces();
+});
