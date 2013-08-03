0ef53c402a3a542bb4db931315a9c6e1522f28da
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-03 14:23:36 -0400

    added route and action for videos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   def index
   end
   def gallery
+  end  
+  def videos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Facesofmadison::Application.routes.draw do
   root to: 'pages#index'
 
   get 'gallery' => 'pages#gallery'
+  get 'videos' => 'pages#videos'
 end
